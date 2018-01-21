568b4cca0158d476ed0e97efd871998316b64e91
unknown <j.quaresmasantos_98@hotmail.com>
2018-01-21 00:44:46 -0300

    Show images

--- views/propriedades/index.php
+++ views/propriedades/index.php
@@ -5,19 +5,26 @@
 	<?php endif;?>
 	<?php foreach ($viewmodel as $item): ?>
 	<div class="row">
+
 		<div class="col s12 m12">
 			<div class="card">
 
-			<img src="<?php echo ROOT_URL ?>showImage.php?id=<?php echo $item['id'] ?>">
+			<div class="mine">
+				<img class="card-img-top" alt="Card image" style="width:100%" src="<?php echo ROOT_PATH; ?>assets/imgs_propriedades/<?php echo $item['nome'] ?>">
+			</div>
+
 				<div class="card-stacked">
 					<div class="card-content">
-						<span class="card-title activator grey-text text-darken-4"><?php echo $item['nome_propriedade'] ?></span>
-						<ul>
-							<li class="staggered-list"><b>Pais: </b><?php echo $item['pais'] ?></li>
-							<li class="staggered-list"><b>Estado: </b><?php echo $item['nome_estado'] ?></li>
-							<li class="staggered-list"><b>Cidade: </b><?php echo $item['nome_cidade'] ?></li>
-							<li class="staggered-list"><b>Area(m<sup>2</sup>): </b><?php echo $item['area'] ?></li>
-						</ul>
+						
+						
+							<span class="card-title activator grey-text text-darken-4"><?php echo $item['nome_propriedade'] ?></span>
+							<ul>
+								<li class="staggered-list"><b>Pais: </b><?php echo $item['pais'] ?></li>
+								<li class="staggered-list"><b>Estado: </b><?php echo $item['nome_estado'] ?></li>
+								<li class="staggered-list"><b>Cidade: </b><?php echo $item['nome_cidade'] ?></li>
+								<li class="staggered-list"><b>Area(m<sup>2</sup>): </b><?php echo $item['area'] ?></li>
+							</ul>
+				
 					</div>
 					<div class="card-action">
 						<a href="#">Editar</a>
